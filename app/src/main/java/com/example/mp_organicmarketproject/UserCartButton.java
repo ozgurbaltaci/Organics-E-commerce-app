0cca7679a7ff0dc04e6e087b4306a1fc43f80a2e
ozgurbaltaci <20180808066@ogr.akdeniz.edu.tr>
2021-04-17 15:56:26 +0300

    Home Page-2 trying

---
+++ app/src/main/java/com/example/mp_organicmarketproject/UserCartButton.java
@@ -0,0 +1,23 @@
+package com.example.mp_organicmarketproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class UserCartButton extends Fragment {
+
+    public UserCartButton(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.usercart_fragment, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/mp_organicmarketproject/UserProfileButton.java
@@ -0,0 +1,23 @@
+package com.example.mp_organicmarketproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class UserProfileButton extends Fragment {
+
+    public UserProfileButton(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.userprofile_fragment, container, false);
+    }
+}
