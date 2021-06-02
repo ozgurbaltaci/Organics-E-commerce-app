067617ece4667334c183c6d971d2c56dada95f98
ozgurbaltaci <20180808066@ogr.akdeniz.edu.tr>
2021-06-02 17:58:20 +0300

    comments will be displayed with comment's owner's email instead of their name+surname because there can be another user with same name + surnmae and it would cause conflict.Fixed it.

--- app/src/main/java/com/example/mp_organicmarketproject/dto/User.java
+++ app/src/main/java/com/example/mp_organicmarketproject/dto/User.java
@@ -8,13 +8,15 @@ public class User {
     public String name;
     public String surname;
     public String phone;
+    public String email;
 
     public User() {
     }
 
-    public User(String name, String surname, String phone) {
+    public User(String name, String surname, String phone, String email) {
         this.name = name;
         this.surname = surname;
         this.phone = phone;
+        this.email = email;
     }
 }
